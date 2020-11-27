259a4069b045b867ee6ac0fa5905a0408ba79d93
Dias <47509783+daudias@users.noreply.github.com>
2020-11-27 23:53:17 +0600

    done 5th lab

---
+++ Student/LearningXcode/LearningXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningXcode
+//
+//  Created by Dias on 11/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var names = ["Timmy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+    }
+
+}
+
