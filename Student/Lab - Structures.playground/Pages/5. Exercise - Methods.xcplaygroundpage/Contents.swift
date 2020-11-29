d349d0687e9d07c739eb62d0d183a7ec9986febb
Dias <47509783+daudias@users.noreply.github.com>
2020-11-29 15:47:58 +0600

    done 11th lab

--- Student/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ Student/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -8,7 +8,14 @@ struct Book {
     var author: String
     var pages: Int
     var price: Double
+    
+    func description() {
+        print("\(title), \(author), \(pages), \(price)")
+    }
 }
+
+var book = Book(title: "Mitio Kaku", author: "Mitio Kaku", pages: 1, price: 1)
+book.description()
 /*:
  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
  */
@@ -16,5 +23,14 @@ struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
+    
+    mutating func like() {
+        likes += 1
+    }
 }
+
+var post = Post(message: "Hello, world!", likes: 10, numberOfComments: 1)
+print(post.likes)
+post.like()
+print(post.likes)
 //: [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
--- Student/Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
+++ Student/Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
@@ -6,16 +6,36 @@
 struct Rectangle {
     var width: Int
     var height: Int
+    var area: Int {
+        width * height
+    }
 }
+
+var rect = Rectangle(width: 11, height: 12)
+print(rect.area)
 /*:
  In the `Height` struct below, height is represented in both inches and centimeters. However, if `heightInInches` is changed, `heightInCentimeters` should also adjust to match it. Add a `didSet` to each property that will check if the other property is what it should be, and if not, sets the proper value. If you set the value of the other property even though it already has the right value, you will end up with an infinite loop of each property setting the other.
  
  Create an instance of `Height` and then change one of its properties. Print out the other property to ensure that it was adjusted accordingly.
  */
 struct Height {
-    var heightInInches: Double
+    var heightInInches: Double {
+        didSet {
+            let updatedHeightInCentimeters = heightInInches * 2.54
+            if heightInCentimeters != updatedHeightInCentimeters {
+                heightInCentimeters = updatedHeightInCentimeters
+            }
+        }
+    }
     
-    var heightInCentimeters: Double
+    var heightInCentimeters: Double {
+        didSet {
+            let updatedHeightInInches = heightInCentimeters / 2.54
+            if heightInInches != updatedHeightInInches {
+                heightInInches = updatedHeightInInches
+            }
+        }
+    }
     
     init(heightInInches: Double) {
         self.heightInInches = heightInInches
@@ -27,4 +47,11 @@ struct Height {
         self.heightInInches = heightInCentimeters/2.54
     }
 }
+
+var myHeight = Height(heightInCentimeters: 181)
+print(myHeight.heightInInches)
+myHeight.heightInCentimeters = 149
+print(myHeight.heightInInches)
+myHeight.heightInInches = 70
+print(myHeight.heightInCentimeters)
 //: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Mile Times and Congratulations](@next)
