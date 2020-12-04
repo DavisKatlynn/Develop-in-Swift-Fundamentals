b41f79bfe07fbc111bc90f5e125993a6113f605a
Dias <47509783+daudias@users.noreply.github.com>
2020-12-04 17:24:40 +0600

    done 23th lab

---
+++ Student/Lab - Apple Pie/Lab - Apple Pie/Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Lab - Apple Pie
+//
+//  Created by Dias on 12/4/20.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += String(letter)
+            } else {
+                guessedWord += " "
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
